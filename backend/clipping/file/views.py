96b13848e02c791352851cc140be64fdb153d304
minyic7 <minyic.20@gmail.com>
2024-11-02 16:59:43 +1100

    feat: Integrate file app with main project
    - Updated `clipping/urls.py` to include routes for the `file` application.
    - Added `FileViewSet` in `file/views.py` to handle CRUD operations for `File` model.
    - Created `file/urls.py` to manage routes for `FileViewSet` using DRF's DefaultRouter.

--- backend/clipping/file/views.py
+++ backend/clipping/file/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import File
+from .serializers import FileSerializer
 
-# Create your views here.
+
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
